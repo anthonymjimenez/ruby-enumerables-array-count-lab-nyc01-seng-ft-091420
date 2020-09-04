f7a6cf9bd8e62de62dca2268e6b68191e769b7da
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-04 23:51:18 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,6 +4,8 @@ def count_strings(array)
     x.isa? String
 end
 
+puts count_strings([2,1,"Ad", "f0", "fg" ])
+
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
